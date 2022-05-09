eea1c7ce4d844091aced1f691c27acf135339d54
Ferina-code <100728266+Ferina-code@users.noreply.github.com>
2022-05-08 21:42:20 -0700

    Assignment Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskList = document.querySelector('form')
+  taskList.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_task.value);
+    taskList.reset();
+  });
 });
+function buildToDo(task) {
+  const p = document.createElement('p');
+  const btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x'
+  p.textContent = `${task} `;
+  console.log(p);
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+} 
